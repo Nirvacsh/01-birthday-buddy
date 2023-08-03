b02e84bdcdf69f513a1df1583d868a4e4969c232
Nirvacsh <nir.vlad.zu@gmail.com>
2023-08-03 16:56:37 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,24 @@
+import List from './List.jsx'
+import data from './data.js'
+import { useState } from 'react'
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
-};
-export default App;
+  const [people, setPeople] = useState(data)
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button
+          type='button'
+          className='btn btn-block'
+          onClick={() => setPeople([])}
+        >
+          clear all
+        </button>
+      </section>
+    </main>
+  )
+}
+export default App
